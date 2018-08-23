b3f8bf93df72dedfea8a3985f7965cafba48b12f
Igor Talankin <italankin@gmail.com>
2018-08-23 08:44:08 +0500

    Add shortcut lnch system settings

--- launcher/src/main/java/com/italankin/lnch/feature/settings_root/SettingsRootFragment.java
+++ launcher/src/main/java/com/italankin/lnch/feature/settings_root/SettingsRootFragment.java
@@ -1,17 +1,29 @@
 package com.italankin.lnch.feature.settings_root;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.italankin.lnch.R;
+import com.italankin.lnch.util.IntentUtils;
 
 public class SettingsRootFragment extends PreferenceFragmentCompat {
     private Callbacks callbacks;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -57,6 +69,26 @@ public class SettingsRootFragment extends PreferenceFragmentCompat {
             }
             return true;
         });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.settings, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_system_settings) {
+            Context context = getContext();
+            Intent intent = IntentUtils.getPackageSystemSettings(context.getPackageName());
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(context, R.string.error, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private Preference findPreference(@StringRes int key) {
