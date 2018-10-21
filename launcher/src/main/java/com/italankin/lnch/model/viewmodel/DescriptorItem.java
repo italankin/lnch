1b790f2ceb08b020985bcde1726e17edff690aa9
Igor Talankin <italankin@gmail.com>
2018-10-22 03:23:08 +0500

    Add some javadoc

--- launcher/src/main/java/com/italankin/lnch/model/viewmodel/DescriptorItem.java
+++ launcher/src/main/java/com/italankin/lnch/model/viewmodel/DescriptorItem.java
@@ -6,7 +6,14 @@ public interface DescriptorItem {
 
     Descriptor getDescriptor();
 
+    /**
+     * @return {@code true} if this item shares the same {@link Descriptor#getId() id}
+     * with {@code another}
+     */
     boolean is(DescriptorItem another);
 
+    /**
+     * @return {@code true}, if this item has the same content as {@code another}
+     */
     boolean deepEquals(DescriptorItem another);
 }
