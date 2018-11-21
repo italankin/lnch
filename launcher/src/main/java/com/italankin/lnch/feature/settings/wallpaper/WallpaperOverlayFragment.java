cab085ba45eb2d4db244dc82f2ee3e799844533a
Igor Talankin <italankin@gmail.com>
2018-11-21 22:48:29 +0500

    Rename fragment_settings_wallpaper_overlay.xml

--- launcher/src/main/java/com/italankin/lnch/feature/settings/wallpaper/WallpaperOverlayFragment.java
+++ launcher/src/main/java/com/italankin/lnch/feature/settings/wallpaper/WallpaperOverlayFragment.java
@@ -60,7 +60,7 @@ public class WallpaperOverlayFragment extends AppFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wallpaper_overlay, container, false);
+        return inflater.inflate(R.layout.fragment_settings_wallpaper_overlay, container, false);
     }
 
     @Override
