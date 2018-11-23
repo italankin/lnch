518002c0e30edc79b6ff7499fcd7c95884a6455d
Igor Talankin <italankin@gmail.com>
2018-11-23 21:17:49 +0500

    Setup StrictMode policy

--- launcher/src/debug/java/com/italankin/lnch/DebugLauncherApp.java
+++ launcher/src/debug/java/com/italankin/lnch/DebugLauncherApp.java
@@ -1,10 +1,22 @@
 package com.italankin.lnch;
 
+import android.os.StrictMode;
+
 import timber.log.Timber;
 
 public class DebugLauncherApp extends LauncherApp {
     @Override
     public void onCreate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectActivityLeaks()
+                .detectLeakedRegistrationObjects()
+                .detectLeakedClosableObjects()
+                .build());
         Timber.plant(new Timber.DebugTree());
         super.onCreate();
     }
