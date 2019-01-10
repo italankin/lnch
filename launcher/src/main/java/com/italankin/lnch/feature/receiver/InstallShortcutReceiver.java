973cc27554701e1a3179b36c0523efa134e64b77
Igor Talankin <i.talankin@tinkoff.ru>
2019-01-10 11:29:44 +0500

    Suppress deprecation warring

--- launcher/src/main/java/com/italankin/lnch/feature/receiver/InstallShortcutReceiver.java
+++ launcher/src/main/java/com/italankin/lnch/feature/receiver/InstallShortcutReceiver.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 
 import timber.log.Timber;
 
+@SuppressWarnings("deprecation")
 public class InstallShortcutReceiver extends BroadcastReceiver {
 
     private static final String ACTION = "com.android.launcher.action.INSTALL_SHORTCUT";
