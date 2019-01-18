69bb33bff7c225eb9eb607231682b1c37354b439
Igor Talankin <i.talankin@tinkoff.ru>
2019-01-18 14:43:37 +0500

    Remove unused method

--- launcher/src/main/java/com/italankin/lnch/LauncherApp.java
+++ launcher/src/main/java/com/italankin/lnch/LauncherApp.java
@@ -1,15 +1,10 @@
 package com.italankin.lnch;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.italankin.lnch.di.service.DaggerService;
 
 public class LauncherApp extends Application {
-
-    public static LauncherApp getInstance(Context context) {
-        return (LauncherApp) context.getApplicationContext();
-    }
 
     public static DaggerService daggerService;
 
