0d5fe5660ec5516196cf58798483d5d39621215a
Igor Talankin <italankin@gmail.com>
2019-06-17 17:41:51 +0500

    Add clarifying comment

--- launcher/src/main/java/com/italankin/lnch/feature/home/HomePresenter.java
+++ launcher/src/main/java/com/italankin/lnch/feature/home/HomePresenter.java
@@ -324,7 +324,7 @@ public class HomePresenter extends AppPresenter<HomeView> {
                 .map(ViewModelFactory::createItems)
                 .doOnNext(this::restoreGroupsState)
                 .scan(Update.EMPTY, this::calculateUpdates)
-                .skip(1);
+                .skip(1); // skip empty update
     }
 
     private Observable<UserPrefs> observeUserPrefs() {
