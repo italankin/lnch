600edd15e1f824e95c553ad5cfc858f4aae5477a
Igor Talankin <italankin@gmail.com>
2020-11-15 22:31:45 +0500

    Enable widgets for all APIs

--- launcher/src/main/java/com/italankin/lnch/feature/settings/widgets/WidgetsSettingsFragment.java
+++ launcher/src/main/java/com/italankin/lnch/feature/settings/widgets/WidgetsSettingsFragment.java
@@ -1,26 +1,14 @@
 package com.italankin.lnch.feature.settings.widgets;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
 import com.italankin.lnch.R;
 import com.italankin.lnch.feature.settings.base.BasePreferenceFragment;
-import com.italankin.lnch.model.repository.prefs.Preferences;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class WidgetsSettingsFragment extends BasePreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.prefs_widgets);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        findPreference(Preferences.ENABLE_WIDGETS).setEnabled(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O);
     }
 }
