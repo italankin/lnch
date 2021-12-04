280b80c796459c63b01d563858ec9a074ac7a99a
Igor Talankin <italankin@gmail.com>
2021-12-05 00:09:15 +0500

    Added base implementation for list filter

---
+++ launcher/src/main/java/com/italankin/lnch/util/filter/ListFilter.java
@@ -0,0 +1,80 @@
+package com.italankin.lnch.util.filter;
+
+import android.text.TextUtils;
+import android.widget.Filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static java.util.Collections.emptyList;
+
+public abstract class ListFilter<T> extends Filter {
+    protected static final FilterResults EMPTY;
+
+    static {
+        EMPTY = new FilterResults();
+        EMPTY.count = 0;
+        EMPTY.values = emptyList();
+    }
+
+    protected final List<T> unfiltered = new CopyOnWriteArrayList<>();
+    @Nullable
+    protected final OnFilterResult<T> onFilterResult;
+
+    protected volatile CharSequence constraint;
+
+    public ListFilter(@Nullable OnFilterResult<T> onFilterResult) {
+        this.onFilterResult = onFilterResult;
+    }
+
+    public void setDataset(@NonNull List<T> dataset) {
+        unfiltered.clear();
+        unfiltered.addAll(dataset);
+        fireFilter();
+    }
+
+    protected abstract FilterResults performFiltering(String query, List<T> unfiltered);
+
+    @Override
+    protected final FilterResults performFiltering(CharSequence constraint) {
+        this.constraint = constraint;
+        String query = constraint.toString().trim().toLowerCase(Locale.getDefault());
+        return performFiltering(query, unfiltered);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected final void publishResults(CharSequence constraint, FilterResults filterResults) {
+        if (onFilterResult != null) {
+            onFilterResult.onFilterResult(constraint == null ? null : constraint.toString(),
+                    (List<T>) filterResults.values);
+        }
+    }
+
+    protected void fireFilter() {
+        if (!TextUtils.isEmpty(constraint)) {
+            filter(constraint);
+        } else {
+            publishResults(null, of(new ArrayList<>(unfiltered)));
+        }
+    }
+
+    protected static <T> FilterResults of(List<T> items) {
+        if (items.size() == 0) {
+            return EMPTY;
+        }
+        FilterResults results = new FilterResults();
+        results.values = items;
+        results.count = items.size();
+        return results;
+    }
+
+    public interface OnFilterResult<T> {
+        void onFilterResult(String query, List<T> items);
+    }
+}
