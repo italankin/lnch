46bed8f0b72154e92d0009a03d546360b8792399
Igor Talankin <italankin@gmail.com>
2021-12-05 21:32:52 +0500

    Fix ComponentSelectorView state strategy

--- launcher/src/main/java/com/italankin/lnch/feature/intentfactory/componentselector/ComponentSelectorView.java
+++ launcher/src/main/java/com/italankin/lnch/feature/intentfactory/componentselector/ComponentSelectorView.java
@@ -1,11 +1,14 @@
 package com.italankin.lnch.feature.intentfactory.componentselector;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.italankin.lnch.feature.intentfactory.componentselector.model.ComponentNameUi;
 
 import java.util.List;
 
 interface ComponentSelectorView extends MvpView {
 
+    @StateStrategyType(SingleStateStrategy.class)
     void onComponentsLoaded(List<ComponentNameUi> componentNames);
 }
