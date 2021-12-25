eca6113b59e6a24ff7265f060e3bf01563792d7e
Igor Talankin <italankin@gmail.com>
2021-12-26 02:14:45 +0500

    Refactor DescriptorArg; add DescriptorArgFragmentResultContract

--- launcher/src/main/java/com/italankin/lnch/model/descriptor/DescriptorArg.java
+++ launcher/src/main/java/com/italankin/lnch/model/descriptor/DescriptorArg.java
@@ -1,26 +1,56 @@
 package com.italankin.lnch.model.descriptor;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public final class DescriptorArg implements Serializable {
+@SuppressWarnings("unchecked")
+public final class DescriptorArg<T extends Descriptor> implements Parcelable {
+
+    public static final Creator<DescriptorArg<?>> CREATOR = new Creator<DescriptorArg<?>>() {
+        @Override
+        public DescriptorArg<?> createFromParcel(Parcel in) {
+            return new DescriptorArg<>(in);
+        }
+
+        @Override
+        public DescriptorArg<?>[] newArray(int size) {
+            return new DescriptorArg[size];
+        }
+    };
 
     public final String id;
-    public final Class<? extends Descriptor> type;
+    public final Class<T> type;
 
-    public DescriptorArg(Descriptor descriptor) {
-        this(descriptor.getId(), descriptor.getClass());
+    public DescriptorArg(T descriptor) {
+        this(descriptor.getId(), (Class<T>) descriptor.getClass());
     }
 
-    public DescriptorArg(String id) {
-        this(id, Descriptor.class);
+    public DescriptorArg(Descriptor descriptor, Class<T> type) {
+        this(descriptor.getId(), type);
     }
 
-    public DescriptorArg(String id, Class<? extends Descriptor> type) {
+    public DescriptorArg(String id, Class<T> type) {
         this.id = id;
         this.type = type;
     }
 
     public boolean is(Descriptor descriptor) {
         return descriptor.getId().equals(id) && type.isAssignableFrom(descriptor.getClass());
+    }
+
+    private DescriptorArg(Parcel in) {
+        id = in.readString();
+        type = (Class<T>) in.readSerializable();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeSerializable(type);
     }
 }
