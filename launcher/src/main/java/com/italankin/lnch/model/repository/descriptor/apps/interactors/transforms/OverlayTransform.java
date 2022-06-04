348e3f54d4eadfafa411b808f269b9987751ead2
Igor Talankin <italankin@gmail.com>
2022-06-04 21:43:40 +0500

    Apply overlay color to folders

--- launcher/src/main/java/com/italankin/lnch/model/repository/descriptor/apps/interactors/transforms/OverlayTransform.java
+++ launcher/src/main/java/com/italankin/lnch/model/repository/descriptor/apps/interactors/transforms/OverlayTransform.java
@@ -2,7 +2,6 @@ package com.italankin.lnch.model.repository.descriptor.apps.interactors.transfor
 
 import com.italankin.lnch.model.descriptor.CustomColorDescriptor;
 import com.italankin.lnch.model.descriptor.Descriptor;
-import com.italankin.lnch.model.descriptor.impl.FolderDescriptor;
 import com.italankin.lnch.model.repository.descriptor.apps.AppsData;
 import com.italankin.lnch.model.repository.descriptor.apps.interactors.PreferencesInteractor;
 import com.italankin.lnch.model.repository.prefs.Preferences;
@@ -14,9 +13,6 @@ public class OverlayTransform implements PreferencesInteractor.Transform {
         if (preferences.get(Preferences.APPS_COLOR_OVERLAY_SHOW)) {
             Integer colorOverlay = preferences.get(Preferences.APPS_COLOR_OVERLAY);
             for (Descriptor item : appsData.items) {
-                if (item instanceof FolderDescriptor) {
-                    continue;
-                }
                 if (item instanceof CustomColorDescriptor) {
                     ((CustomColorDescriptor) item).setCustomColor(colorOverlay);
                 }
