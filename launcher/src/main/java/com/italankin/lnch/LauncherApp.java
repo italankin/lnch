d69713495f236f89300e0648a401b001935863da
Igor Talankin <italankin@gmail.com>
2023-11-26 23:50:24 +0400

    Refactor debug build

--- launcher/src/main/java/com/italankin/lnch/LauncherApp.java
+++ launcher/src/main/java/com/italankin/lnch/LauncherApp.java
@@ -1,9 +1,9 @@
 package com.italankin.lnch;
 
 import android.app.Application;
-
 import com.italankin.lnch.di.service.DaggerService;
 import com.italankin.lnch.util.ThemeActivityCallbacks;
+import timber.log.Timber;
 
 public class LauncherApp extends Application {
 
@@ -11,6 +11,9 @@ public class LauncherApp extends Application {
 
     @Override
     public void onCreate() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
         super.onCreate();
         daggerService = new DaggerService(this);
         registerActivityLifecycleCallbacks(new ThemeActivityCallbacks());
