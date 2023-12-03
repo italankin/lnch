2f1bfc11d6a1c34e6b7e317adaa844b247bf7b0b
Igor Talankin <italankin@gmail.com>
2023-12-04 01:37:01 +0400

    Added resize mode flag for WidgetResizeFrame

--- launcher/src/main/java/com/italankin/lnch/feature/widgets/util/WidgetResizeFrame.java
+++ launcher/src/main/java/com/italankin/lnch/feature/widgets/util/WidgetResizeFrame.java
@@ -1,12 +1,27 @@
 package com.italankin.lnch.feature.widgets.util;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 
 public class WidgetResizeFrame extends FrameLayout {
 
+    private boolean resizeMode = false;
+
     public WidgetResizeFrame(@NonNull Context context) {
         super(context);
+    }
+
+    public void setResizeMode(boolean resizeMode) {
+        if (this.resizeMode != resizeMode) {
+            this.resizeMode = resizeMode;
+            invalidate();
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return resizeMode;
     }
 }
