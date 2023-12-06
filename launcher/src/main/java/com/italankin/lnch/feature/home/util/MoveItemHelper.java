8f06768c9c2b33edd32b64a9fb53ac03af471230
Igor Talankin <italankin@gmail.com>
2023-12-07 01:43:07 +0400

    Disable swapping of invisible items

--- launcher/src/main/java/com/italankin/lnch/feature/home/util/MoveItemHelper.java
+++ launcher/src/main/java/com/italankin/lnch/feature/home/util/MoveItemHelper.java
@@ -3,6 +3,9 @@ package com.italankin.lnch.feature.home.util;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import com.italankin.lnch.feature.home.adapter.HomeAdapter;
+import com.italankin.lnch.model.ui.DescriptorUi;
+import com.italankin.lnch.model.ui.IgnorableDescriptorUi;
 
 public class MoveItemHelper extends ItemTouchHelper.Callback {
 
@@ -20,6 +23,14 @@ public class MoveItemHelper extends ItemTouchHelper.Callback {
             @NonNull RecyclerView.ViewHolder target) {
         callback.onItemMove(viewHolder.getBindingAdapterPosition(), target.getBindingAdapterPosition());
         return true;
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    @Override
+    public boolean canDropOver(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder current,
+            @NonNull RecyclerView.ViewHolder target) {
+        DescriptorUi item = ((HomeAdapter) recyclerView.getAdapter()).getItem(target.getBindingAdapterPosition());
+        return !(item instanceof IgnorableDescriptorUi) || !((IgnorableDescriptorUi) item).isIgnored();
     }
 
     @Override
